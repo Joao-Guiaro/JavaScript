fc7a9438e400789251b609d23539de95aa01e710
Joao-Guiaro <guiarojv@gmail.com>
2026-03-03 14:33:11 -0300

    Studing JS

---
+++ javascript/dom/03.03.26/atividade.js
@@ -0,0 +1,7 @@
+let titulo = document.querySelector("#titulo");
+let azul = document.querySelector("#azul");
+let vermelho = document.querySelector("#vermelho");
+
+azul.addEventListener("click", function(){
+    texto.style.color = "azul";
+});
